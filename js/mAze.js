d173042d033af9a8d0321df0824aa9dd35f35c17
Armaan Ahluwalia <armaanahluwalia@hotmail.com>
2012-04-23 17:30:21 -0400

    added ie support

--- js/mAze.js
+++ js/mAze.js
@@ -14,27 +14,65 @@
       'gray' : {
         effect : 'desaturate',
         options : null,
-        timing : 200
+        timing : 200,
+				ie : {
+					'filter' : 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)'
+				}
       }
     },
+		_isIE : null,
     _options : null,
     init: function(options, elem) {
 			var self = this;
 			
 			this._options = $.extend({}, this.defaults, options);
-			
+			this._isIE = (this.isIE() == -1) ? false : true;	
 			$('*[data-btn]').each(function() {
+				
 				var $btn = $(this).addClass('mAze_btn');
-			  var effectName = $btn.attr('data-btn');			  
-				var dfd = self.setupImages(this, effectName);
-				(function(btn, effectName) {
-  				dfd.done(function() {
-  				  self.setupHover(btn, effectName);
-  				});				  
-				})($btn, effectName);
+			  var effectName = $btn.attr('data-btn');
+			
+				if(!self._isIE) {
+				//If its not IE
+					var dfd = self.setupImages(this, effectName);
+					(function(btn, effectName) {
+	  				dfd.done(function() {
+	  				  self.setupHover(btn, effectName);
+	  				});				  
+					})($btn, effectName);
+					
+				}	else {
+				//If it is IE
+					self.setupIE($btn, effectName);
+				}
+				
 			});
       return this;
     },
+		setupIE : function($btn, effectName) {
+			var effects = this._options[effectName]['ie'];
+			var self= this;
+			if(effects) {
+				var theClass = (self._options[effectName]['reverse']) ? 'mAze_u' : 'mAze_o';
+				var theOtherClass = (self._options[effectName]['reverse']) ? 'mAze_o' : 'mAze_u';
+				$underBtn = $('img',$btn).first().addClass(theClass);
+				
+				$btn.css({ 'width' : $underBtn.width(), 'height' : $underBtn.height(), 'display': 'inline-block' })				
+				
+				$overBtn = $underBtn.clone().removeClass(theClass).addClass(theOtherClass).appendTo($btn);
+				for (var i in effects) {
+					$('.mAze_o', $btn).css(i, effects[i]);
+				}				
+	      $btn.hover(
+	        function() {
+	          $('.' + theClass, this).animate({opacity:0}, self._options[effectName]['timing']);
+	        },
+	        function() {
+	          $('.' + theClass, this).animate({opacity:1}, self._options[effectName]['timing']);
+	        }
+				);
+			}
+		},
     setupHover : function(container, effect) {
       var self = this;
       $(container).hover(
@@ -99,6 +137,17 @@
 		  $newImg = $(img).clone().removeClass(theRemoveClass).appendTo($container);
       $newImg.pixastic(effectData['effect'], effectData['options']).addClass(theClass);
 		  dfd.resolve();
+		},
+		isIE : function() {
+		  var rv = -1; // Return value assumes failure.
+		  if (navigator.appName == 'Microsoft Internet Explorer')
+		  {
+		    var ua = navigator.userAgent;
+		    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+		    if (re.exec(ua) != null)
+		      rv = parseFloat( RegExp.$1 );
+		  }
+		  return rv;
 		}
   }
 
